153a69891925c0495d6d0c7f7ea4d6691a3c143c
vecods-source <saaddevlife@gmail.com>
2025-02-17 12:53:34 +0300

    added the SSL certifcate to the database configeration fixing database connection issues and added testing logging

--- config/dp.js
+++ config/dp.js
@@ -9,6 +9,18 @@ const pool = new pg.Pool({
   database: process.env.DB_NAME || "default_db",
   password: process.env.DB_PASSWORD || "default_password",
   port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+pool
+  .connect()
+  .then(() => {
+    console.log("Connected to the database securely using SSL");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+  });
 
 export default pool;
