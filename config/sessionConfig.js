43d19bbd1ea8a652ac3f6e860837106e209d77b3
vecods-source <saaddevlife@gmail.com>
2025-02-18 13:48:06 +0300

    modifying the max age i found out that sessions expired as soon as it created

--- config/sessionConfig.js
+++ config/sessionConfig.js
@@ -11,7 +11,7 @@ const sessionConfig = session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: true, httpOnly: true, maxAge: 3600000, sameSite: "none" },
+  cookie: { secure: true, httpOnly: true, maxAge: null, sameSite: "none" },
 });
 
 export default sessionConfig;
